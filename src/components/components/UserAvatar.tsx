ab24cc5730811dc12b75b1417059963f7f516d3c
Anton JSN <a.franchuk.jsn@gmail.com>
2021-08-05 16:04:07 +0300

    Write test for BackButton and ProfileButton components.

--- src/components/components/UserAvatar.tsx
+++ src/components/components/UserAvatar.tsx
@@ -1,55 +1,48 @@
-import React, { useState } from 'react'
-import {
-  View,
-  StyleSheet,
-  Image,
-  ImageSourcePropType
-} from "react-native"
-import { AntDesign } from '@expo/vector-icons'
+import React, { useState } from "react";
+import { View, StyleSheet, Image, ImageSourcePropType } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 interface UserAvatarPropsInterface {
-  size: string
-  url?: string
+  size: string;
+  url?: string;
 }
 
 export default function UserAvatar({ size, url }: UserAvatarPropsInterface) {
   return (
-    <View style={
-      size === 'small'
-        ? styles.containerSmall
-        : styles.containerBig
-    }>
-      {
-        url === undefined
-          ? <AntDesign name="user" size={75} color="black" />
-          : <Image source={{ uri: url }} style={styles.image} />
-      }
-    </ View>
-  )
+    <View
+      style={size === "small" ? styles.containerSmall : styles.containerBig}
+    >
+      {url === undefined ? (
+        <AntDesign name="user" size={75} color="black" />
+      ) : (
+        <Image source={{ uri: url }} style={styles.image} />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   containerSmall: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 8,
     width: 100,
     height: 100,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 32,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
   containerBig: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 8,
-    width: '100%',
+    width: "100%",
     height: 300,
-    backgroundColor: '#fff',
-    overflow: 'hidden'
+    backgroundColor: "#fff",
+    overflow: "hidden",
   },
   image: {
-    width: '100%',
-    height: '100%',
-  }
-})
+    width: "100%",
+    height: "100%",
+  },
+});
