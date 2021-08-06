a0641a743760129e7b09465ff3ae342e6794e8b2
Sergey T <sergei.timoshchenko.jsn@gmail.com>
2021-08-06 22:29:35 +0300

    SNAP-190 translator refined

--- src/translator/i18n.ts
+++ src/translator/i18n.ts
@@ -4,14 +4,14 @@ import {useAppSelector} from "../store/hooks";
 import {LANGUAGE_ENGLISH} from "../store/modules/profile/constants";
 import { translate } from './Translator'
 
-type TType = (key: string) => string;
+type TType = (key: string, args: any) => string;
 
 export class Translator {
     protected static instance: Translator | undefined;
     public tranlator: TType;
 
     constructor() {
-        this.tranlator = (key) => translate(key, en)
+        this.tranlator = (key, args) => translate(key, args, en)
     }
 
     static getInstance() {
@@ -23,8 +23,8 @@ export class Translator {
         return this.instance
     }
 
-    public trans(key: string): string {
-        return this.tranlator(key)
+    public trans(key: string, args: any): string {
+        return this.tranlator(key, args)
     }
 
     public getTranslator() {
@@ -40,7 +40,7 @@ export class Translator {
 export function useI18n() {
     const singleton = Translator.getInstance()
     const currentLanguage = useAppSelector(state => state.profile.language)
-    const t: TType = (key: string) => translate(key, currentLanguage === LANGUAGE_ENGLISH ? en : de)
+    const t: TType = (key: string, args: any) => translate(key, args, currentLanguage === LANGUAGE_ENGLISH ? en : de)
     singleton.setTranslator(t)
     return {t}
 }
