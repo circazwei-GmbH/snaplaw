17b7d57c49ac1f627e676162a156539c25b114c5
Anton JSN <a.franchuk.jsn@gmail.com>
2021-08-10 22:07:14 +0300

    Fix text check in MyProfile test.

--- src/components/pages/__tests__/MyProfile.test.tsx
+++ src/components/pages/__tests__/MyProfile.test.tsx
@@ -6,8 +6,8 @@ import * as RootNavigation from "../../../router/RootNavigation";
 import store from "../../../store/index";
 import { createStore } from "@reduxjs/toolkit";
 import { toggleBoolValue } from "../../../utils/toggleBoolValue";
-import {PROFILE_ROUTER} from "../../../router/ProfileRouterTypes";
-import {killToken} from "../../../store/modules/auth/slice";
+import { PROFILE_ROUTER } from "../../../router/ProfileRouterTypes";
+import { killToken } from "../../../store/modules/auth/slice";
 
 jest.mock("../../../utils/toggleBoolValue.ts");
 
@@ -18,14 +18,25 @@ afterEach(() => {
   jest.restoreAllMocks();
 });
 
-const actions = jest.fn()
+const actions = jest.fn();
 
 const reduser = (state: any, action: any): any => {
-  actions(action)
-  return state
-}
+  actions(action);
+  return state;
+};
 
-const customStore = createStore(reduser)
+const customStore = createStore(reduser);
+
+const titleAndButtonsText = [
+  "my_profile.buttons_text.my_profile",
+  "my_profile.buttons_text.language",
+  "my_profile.buttons_text.notifications",
+  "my_profile.buttons_text.invite_friends",
+  "my_profile.buttons_text.private_policy",
+  "my_profile.buttons_text.change_password",
+  "my_profile.buttons_text.sign_out",
+  "my_profile.buttons_text.delete_profile",
+];
 
 describe("MyProfile", () => {
   it("Should display page title and buttons text", () => {
@@ -35,16 +46,9 @@ describe("MyProfile", () => {
       </Provider>
     );
 
-    expect(getByText("my_profile.headline")).toBeTruthy();
-
-    expect(getByText("my_profile.buttons_text.my_profile")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.language")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.notifications")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.invite_friends")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.private_policy")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.change_password")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.sign_out")).toBeTruthy();
-    expect(getByText("my_profile.buttons_text.delete_profile")).toBeTruthy();
+    titleAndButtonsText.map((item) => {
+      expect(getByText(item)).toBeTruthy();
+    });
   });
 
   it("Press on avatar should toggle avatar size", () => {
@@ -69,17 +73,19 @@ describe("MyProfile", () => {
     expect(RootNavigation.navigate).toBeCalledWith(PROFILE_ROUTER.EDIT_PROFILE);
 
     fireEvent.press(getByTestId("MyProfile.language"));
-    expect(RootNavigation.navigate).toBeCalledWith(PROFILE_ROUTER.CHANGE_LANGUAGE);
+    expect(RootNavigation.navigate).toBeCalledWith(
+      PROFILE_ROUTER.CHANGE_LANGUAGE
+    );
   });
 
-  it('Should dispatch clear token (logout functionality)', () => {
+  it("Should dispatch clear token (logout functionality)", () => {
     const { getByText } = render(
-        <Provider store={customStore} >
-          <MyProfile />
-        </Provider>
-    )
+      <Provider store={customStore}>
+        <MyProfile />
+      </Provider>
+    );
 
-    fireEvent.press(getByText('my_profile.buttons_text.sign_out'))
-    expect(actions).toBeCalledWith(killToken())
-  })
+    fireEvent.press(getByText("my_profile.buttons_text.sign_out"));
+    expect(actions).toBeCalledWith(killToken());
+  });
 });
