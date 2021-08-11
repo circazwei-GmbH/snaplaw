875191a17fea0cbbef70d43ce06cc7eb1a632004
Sergey T <sergei.timoshchenko.jsn@gmail.com>
2021-08-11 13:30:11 +0300

    SNAP-191 test fixed

--- src/components/pages/__tests__/MyProfile.test.tsx
+++ src/components/pages/__tests__/MyProfile.test.tsx
@@ -7,7 +7,7 @@ import store from "../../../store/index";
 import { createStore } from "@reduxjs/toolkit";
 import { toggleBoolValue } from "../../../utils/toggleBoolValue";
 import { PROFILE_ROUTER } from "../../../router/ProfileRouterTypes";
-import { killToken } from "../../../store/modules/auth/slice";
+import {clearToken} from "../../../store/modules/auth/action-creators";
 
 jest.mock("../../../utils/toggleBoolValue.ts");
 
@@ -86,6 +86,6 @@ describe("MyProfile", () => {
     );
 
     fireEvent.press(getByText("my_profile.buttons_text.sign_out"));
-    expect(actions).toBeCalledWith(killToken());
+    expect(actions).toBeCalledWith(clearToken());
   });
 });
