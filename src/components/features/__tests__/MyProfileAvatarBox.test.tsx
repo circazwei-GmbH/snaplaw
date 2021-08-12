11c2eeec02afe231797b47b46e62f3381e3288c1
Sergey T <sergei.timoshchenko.jsn@gmail.com>
2021-08-12 12:13:32 +0300

    SNAP-213 tests fixed

--- src/components/features/__tests__/MyProfileAvatarBox.test.tsx
+++ src/components/features/__tests__/MyProfileAvatarBox.test.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react-native";
 import MyProfileAvatarBox from "../MyProfileAvatarBox";
 import { toggleBoolValue } from "../../../utils/toggleBoolValue";
+import {Provider} from "react-redux";
+import store from "../../../store";
 
 jest.mock("../../../utils/toggleBoolValue.ts");
 
 describe("MyProfileAvatarBox", () => {
   it("Should toggle avatar size", () => {
-    const { getByTestId } = render(<MyProfileAvatarBox />);
+    const { getByTestId } = render(
+        <Provider store={store} >
+          <MyProfileAvatarBox />
+        </Provider>
+    );
 
     fireEvent.press(getByTestId("MyProfileAvatarBox.toggle"));
     expect(toggleBoolValue).toBeCalled();
