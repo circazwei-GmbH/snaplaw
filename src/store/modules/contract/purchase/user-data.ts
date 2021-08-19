a82f6d07c33fb580c4a7d7762c332bf44a182dcd
Sergey T <sergei.timoshchenko.jsn@gmail.com>
2021-08-19 21:29:14 +0300

    CON-92 product condition screen added

--- src/store/modules/contract/purchase/user-data.ts
+++ src/store/modules/contract/purchase/user-data.ts
@@ -1,24 +1,24 @@
-import {CONTRACT_SCREEN_TYPES} from "../constants";
+import { CONTRACT_SCREEN_TYPES } from "../constants";
 
 export const enum USER_DATA_FIELDS {
-    name = "name",
-    lastName = "lastName",
-    dateOfBirth = "dateOfBirth",
-    email = "email",
-    phone = "phone",
-    address = "address",
-    postCode = "postCode",
+  name = "name",
+  lastName = "lastName",
+  dateOfBirth = "dateOfBirth",
+  email = "email",
+  phone = "phone",
+  address = "address",
+  postCode = "postCode",
 }
 
 export interface UserDataScreenInterface {
-    type: CONTRACT_SCREEN_TYPES.USER_DATA;
-    data: {
-        [USER_DATA_FIELDS.name]: string;
-        [USER_DATA_FIELDS.lastName]: string;
-        [USER_DATA_FIELDS.dateOfBirth]: string;
-        [USER_DATA_FIELDS.email]: string;
-        [USER_DATA_FIELDS.phone]: string;
-        [USER_DATA_FIELDS.address]: string;
-        [USER_DATA_FIELDS.postCode]: string;
-    };
+  type: CONTRACT_SCREEN_TYPES.USER_DATA;
+  data: {
+    [USER_DATA_FIELDS.name]: string;
+    [USER_DATA_FIELDS.lastName]: string;
+    [USER_DATA_FIELDS.dateOfBirth]: string;
+    [USER_DATA_FIELDS.email]: string;
+    [USER_DATA_FIELDS.phone]: string;
+    [USER_DATA_FIELDS.address]: string;
+    [USER_DATA_FIELDS.postCode]: string;
+  };
 }
