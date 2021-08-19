a9d794b8ec16af49897beff124c840274bb368fe
Anton JSN <a.franchuk.jsn@gmail.com>
2021-08-19 18:18:21 +0300

    Fix contractCardTest.

--- src/components/components/__tests__/ContractTypeCard.test.tsx
+++ src/components/components/__tests__/ContractTypeCard.test.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import {render} from "@testing-library/react-native";
+import React from "react";
+import { render } from "@testing-library/react-native";
 import ContractTypeCard from "../ContractTypeCard";
+import { Provider } from "react-redux";
+import store from "../../../store/index";
 
-const CARD_TEXT = 'card-text';
+const CARD_TEXT = "card-text";
 
-describe('ContractTypeCard', () => {
-    it('Text should be displayed', () => {
-        const { getByText, getByTestId } = render(<ContractTypeCard image={require('../../../../assets/purchase_contract.png')} title={CARD_TEXT} />)
-        expect(getByText(CARD_TEXT)).toBeTruthy()
-        expect(getByTestId(`Image.${CARD_TEXT}`)).toBeTruthy()
-    })
-})
+describe("ContractTypeCard", () => {
+  it("Text should be displayed", () => {
+    const { getByText, getByTestId } = render(
+      <Provider store={store}>
+        <ContractTypeCard
+          image={require("../../../../assets/purchase_contract.png")}
+          title={CARD_TEXT}
+        />
+      </Provider>
+    );
+    expect(getByText(CARD_TEXT)).toBeTruthy();
+    expect(getByTestId(`Image.${CARD_TEXT}`)).toBeTruthy();
+  });
+});
