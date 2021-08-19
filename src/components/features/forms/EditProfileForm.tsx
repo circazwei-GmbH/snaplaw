58626c1b774355110b106a63e417d72ee0bcf63b
Anton JSN <a.franchuk.jsn@gmail.com>
2021-08-19 09:44:45 +0300

    Add KeyboardAwereScrollView to edit profile form. Add awere scroll view to jest config.

--- src/components/features/forms/EditProfileForm.tsx
+++ src/components/features/forms/EditProfileForm.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet } from "react-native";
 import EditProfileTextField from "../../components/EditProfileTextField";
 import { useI18n } from "../../../translator/i18n";
 import { FieldInterface } from "../../features/forms/SignInForm";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export interface EditProfileFieldInterface extends FieldInterface {
   value?: string;
@@ -32,60 +33,62 @@ export default function EditProfileForm({
   const { t } = useI18n();
 
   return (
-    <View style={styles.inputBox}>
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.name")}
-        value={form.name.value}
-        editable={edit}
-        edit={edit}
-        textContentType="username"
-        onChangeFunction={(newValue) => onChangeHandler(newValue, "name")}
-      />
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.lastName")}
-        value={form.lastName.value}
-        editable={edit}
-        edit={edit}
-        onChangeFunction={(newValue) => onChangeHandler(newValue, "lastName")}
-      />
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.dateOfBirth")}
-        value={form.dateOfBirth.value}
-        editable={edit}
-        edit={edit}
-        onChangeFunction={(newValue) =>
-          onChangeHandler(newValue, "dateOfBirth")
-        }
-      />
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.email")}
-        value={form.email.value}
-        editable={false}
-        edit={edit}
-        onChangeFunction={(newValue) => onChangeHandler(newValue, "email")}
-      />
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.phone")}
-        value={form.phone.value}
-        editable={edit}
-        edit={edit}
-        onChangeFunction={(newValue) => onChangeHandler(newValue, "phone")}
-      />
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.address")}
-        value={form.address.value}
-        editable={edit}
-        edit={edit}
-        onChangeFunction={(newValue) => onChangeHandler(newValue, "address")}
-      />
-      <EditProfileTextField
-        placeholder={t("edit_profile.placeholders.postCode")}
-        value={form.postCode.value}
-        editable={edit}
-        edit={edit}
-        onChangeFunction={(newValue) => onChangeHandler(newValue, "postCode")}
-      />
-    </View>
+    <KeyboardAwareScrollView>
+      <View style={styles.inputBox}>
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.name")}
+          value={form.name.value}
+          editable={edit}
+          edit={edit}
+          textContentType="username"
+          onChangeFunction={(newValue) => onChangeHandler(newValue, "name")}
+        />
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.lastName")}
+          value={form.lastName.value}
+          editable={edit}
+          edit={edit}
+          onChangeFunction={(newValue) => onChangeHandler(newValue, "lastName")}
+        />
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.dateOfBirth")}
+          value={form.dateOfBirth.value}
+          editable={edit}
+          edit={edit}
+          onChangeFunction={(newValue) =>
+            onChangeHandler(newValue, "dateOfBirth")
+          }
+        />
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.email")}
+          value={form.email.value}
+          editable={false}
+          edit={edit}
+          onChangeFunction={(newValue) => onChangeHandler(newValue, "email")}
+        />
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.phone")}
+          value={form.phone.value}
+          editable={edit}
+          edit={edit}
+          onChangeFunction={(newValue) => onChangeHandler(newValue, "phone")}
+        />
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.address")}
+          value={form.address.value}
+          editable={edit}
+          edit={edit}
+          onChangeFunction={(newValue) => onChangeHandler(newValue, "address")}
+        />
+        <EditProfileTextField
+          placeholder={t("edit_profile.placeholders.postCode")}
+          value={form.postCode.value}
+          editable={edit}
+          edit={edit}
+          onChangeFunction={(newValue) => onChangeHandler(newValue, "postCode")}
+        />
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
