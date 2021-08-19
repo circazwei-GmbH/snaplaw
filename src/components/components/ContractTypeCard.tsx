bcab381234a2c53227ccd6936d2dab01a3fee61a
Anton JSN <a.franchuk.jsn@gmail.com>
2021-08-19 16:35:13 +0300

    Add style changes to homescreen to fix first contract image position and to change the width of text depending on language.

--- src/components/components/ContractTypeCard.tsx
+++ src/components/components/ContractTypeCard.tsx
@@ -1,42 +1,65 @@
-import React from 'react';
-import {Text, View, StyleSheet, Image, Dimensions, Pressable, GestureResponderEvent} from "react-native";
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  GestureResponderEvent,
+} from "react-native";
+import { useAppSelector } from "../../store/hooks";
+import { LANGUAGE_ENGLISH } from "../../store/modules/profile/constants";
 
 type ContractTypeCardProps = {
-    image: any,
-    title: string,
-    onPress?: (event: GestureResponderEvent) => void
-}
+  image: any;
+  title: string;
+  imageMargin?: object;
+  onPress?: (event: GestureResponderEvent) => void;
+};
 
-export default function ContractTypeCard({ image, title, onPress } : ContractTypeCardProps) {
-    return (
-        <Pressable onPress={onPress}>
-            <View style={styles.container}>
-                <Image source={image} testID={`Image.${title}`} />
-                <Text style={styles.text}>{title}</Text>
-            </View>
-        </Pressable>
-    )
+export default function ContractTypeCard({
+  image,
+  title,
+  onPress,
+  imageMargin,
+}: ContractTypeCardProps) {
+  const currentLanguage = useAppSelector((state) => state.profile.language);
+
+  return (
+    <Pressable onPress={onPress} style={styles.container}>
+      <Image source={image} testID={`Image.${title}`} style={imageMargin} />
+      <Text
+        style={[
+          styles.text,
+          currentLanguage === LANGUAGE_ENGLISH ? styles.widthEng : null,
+        ]}
+      >
+        {title}
+      </Text>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        height: 220,
-        backgroundColor: '#EFF7FD',
-        margin: 3,
-        justifyContent: 'center',
-        alignItems: "center",
-        borderWidth: 1,
-        borderColor: '#E1EEF6',
-        borderRadius: 10,
-        width: Dimensions.get('window').width / 2 - 16,
-    },
-    text: {
-        marginTop: 12,
-        textAlign: 'center',
-        width: 140,
-        color: '#202020',
-        fontSize: 18,
-        fontFamily: 'OS'
-    }
-})
+  container: {
+    flexDirection: "column",
+    width: "48%",
+    height: 220,
+    backgroundColor: "#EFF7FD",
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#E1EEF6",
+    borderRadius: 10,
+  },
+  text: {
+    marginTop: 12,
+    textAlign: "center",
+    color: "#202020",
+    fontSize: 18,
+    fontFamily: "OS",
+  },
+  widthEng: {
+    width: 110,
+  },
+});
--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -1,76 +1,82 @@
-import React from 'react'
-import {View, StyleSheet, ScrollView} from "react-native"
+import React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ContractTypeCard from "../components/ContractTypeCard";
-import {useI18n} from "../../translator/i18n";
+import { useI18n } from "../../translator/i18n";
 import TopBar from "../layouts/TopBar";
 import NotificationBell from "../components/NotificationBell";
 import TextField from "../components/TextField";
 import DefaultText from "../basics/typography/DefaultText";
-import {useAppDispatch} from "../../store/hooks";
-import {requestCreateContract} from "../../store/modules/contract/action-creators";
-import {CONTRACT_TYPES} from "../../store/modules/contract/constants";
+import { useAppDispatch } from "../../store/hooks";
+import { requestCreateContract } from "../../store/modules/contract/action-creators";
+import { CONTRACT_TYPES } from "../../store/modules/contract/constants";
 
-export default function Homepage() {
-    const { t } = useI18n()
-    const dispatch = useAppDispatch()
+export default function Homepage(): JSX.Element {
+  const { t } = useI18n();
+  const dispatch = useAppDispatch();
 
-    return (
-        <TopBar
-            pageName={t('homepage.tab_name')}
-            leftButton={<NotificationBell />}
-        >
-            <>
-                <View style={styles.container}>
-                    <TextField
-                        onChangeFunction={() => {}}
-                        placeholder={t('homepage.search')}
-                    />
-                    <DefaultText text={t('homepage.description')} style={styles.description} />
-                </View>
-                <ScrollView>
-                    <View style={styles.cardContainer}>
-                        <ContractTypeCard
-                            image={require('../../../assets/purchase_contract.png')}
-                            title={t('homepage.contract_types.purchase')}
-                            onPress={() => dispatch(requestCreateContract(CONTRACT_TYPES.PURCHASE))}
-                        />
-                        <ContractTypeCard
-                            image={require('../../../assets/car_contract.png')}
-                            title={t('homepage.contract_types.car')}
-                        />
-                        <ContractTypeCard
-                            image={require('../../../assets/work_contract.png')}
-                            title={t('homepage.contract_types.work')}
-                        />
-                        <ContractTypeCard
-                            image={require('../../../assets/free_contract.png')}
-                            title={t('homepage.contract_types.free')}
-                        />
-                        <ContractTypeCard
-                            image={require('../../../assets/rental_contract.png')}
-                            title={t('homepage.contract_types.rental')}
-                        />
-                    </View>
-                </ScrollView>
-            </>
-        </TopBar>
-    )
+  return (
+    <TopBar pageName={t("homepage.tab_name")} leftButton={<NotificationBell />}>
+      <>
+        <View style={styles.container}>
+          <TextField
+            onChangeFunction={() => {}}
+            placeholder={t("homepage.search")}
+          />
+          <DefaultText
+            text={t("homepage.description")}
+            style={styles.description}
+          />
+        </View>
+        <ScrollView>
+          <View style={styles.cardContainer}>
+            <ContractTypeCard
+              image={require("../../../assets/purchase_contract.png")}
+              title={t("homepage.contract_types.purchase")}
+              onPress={() =>
+                dispatch(requestCreateContract(CONTRACT_TYPES.PURCHASE))
+              }
+              imageMargin={styles.imageMargin}
+            />
+            <ContractTypeCard
+              image={require("../../../assets/car_contract.png")}
+              title={t("homepage.contract_types.car")}
+            />
+            <ContractTypeCard
+              image={require("../../../assets/work_contract.png")}
+              title={t("homepage.contract_types.work")}
+            />
+            <ContractTypeCard
+              image={require("../../../assets/free_contract.png")}
+              title={t("homepage.contract_types.free")}
+            />
+            <ContractTypeCard
+              image={require("../../../assets/rental_contract.png")}
+              title={t("homepage.contract_types.rental")}
+            />
+          </View>
+        </ScrollView>
+      </>
+    </TopBar>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 11,
-        marginTop: 20
-    },
-    description: {
-        marginTop: 34,
-        marginBottom: 7
-    },
-    cardContainer: {
-        flexWrap: 'wrap',
-        flexDirection: "row",
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        margin: 8,
-    }
-})
+  container: {
+    width: "100%",
+    paddingHorizontal: 16,
+  },
+  description: {
+    marginTop: 34,
+    marginBottom: 7,
+  },
+  cardContainer: {
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    padding: 16,
+  },
+  imageMargin: {
+    marginLeft: "12%",
+  },
+});
