c503fef1f7ba6c9e14637cb445b192e9cacb4981
Anton JSN <a.franchuk.jsn@gmail.com>
2021-09-02 15:45:30 +0300

    Add style to notification page container and title.

--- src/components/pages/settings/Notifications.tsx
+++ src/components/pages/settings/Notifications.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import TopBar from "../../layouts/TopBar";
 import { useI18n } from "../../../translator/i18n";
 
 export default function Notifications(): JSX.Element {
   const { t } = useI18n();
 
-  return <TopBar pageName={t("notifications.title")}></TopBar>;
+  return (
+    <TopBar pageName={t("notifications.title")}>
+      <View style={styles.container}>
+        <View style={styles.titleBox}></View>
+      </View>
+    </TopBar>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleBox: {
+    justifyContent: "center",
+    width: "100%",
+    height: 80,
+    paddingHorizontal: 16,
+  },
+});
