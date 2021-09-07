c3f21c1ba6f7673ff049a9a8fe97d4fbf53bd812
Anton JSN <a.franchuk.jsn@gmail.com>
2021-09-07 11:26:05 +0300

    Add swipeable element and on swipe action.

--- src/components/components/NotificationListItem.tsx
+++ src/components/components/NotificationListItem.tsx
@@ -1,60 +1,88 @@
-import React, { useState } from "react";
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, StyleSheet } from "react-native";
 import { NotificationListInterface } from "../pages/settings/Notifications";
 import DefaultText from "../basics/typography/DefaultText";
 import dayjs from "dayjs";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { AntDesign } from "@expo/vector-icons";
 
 interface NotificationListItemPropsInterface {
   item: NotificationListInterface;
   style: object;
 }
 
+const RightSwipeAction = () => (
+  <View style={styles.rightSwipe}>
+    <AntDesign name="checksquareo" size={24} color="#fff" />
+    <DefaultText text={"Read"} style={styles.rightSwipeText} />
+  </View>
+);
+
 export default function NotificationListItem({
   item,
   style,
 }: NotificationListItemPropsInterface): JSX.Element {
+  const swipeable: any = useRef();
   const { isNew, notification, createdAt, userIdFrom } = item;
   const isToday = require("dayjs/plugin/isToday");
   dayjs.extend(isToday);
+  const [isRead, setIsRead] = useState(isNew);
+
+  const onClose = () => {
+    setIsRead(false);
+  };
 
   return (
-    <TouchableOpacity style={style} activeOpacity={0.7}>
-      <View
-        style={[
-          styles.container,
-          isNew ? styles.containerBackgroundNew : styles.containerBackground,
-        ]}
+    <Swipeable
+      renderRightActions={RightSwipeAction}
+      onSwipeableClose={onClose}
+      friction={2}
+      overshootRight={false}
+      ref={swipeable}
+    >
+      <TouchableOpacity
+        style={style}
+        activeOpacity={0.9}
+        onPress={() => console.log("Button pressed")}
       >
-        {isNew ? (
-          <View style={styles.pinkDotBox}>
-            <View style={styles.pinkDot} />
-          </View>
-        ) : null}
-        <View style={styles.notificationBox}>
-          <View style={styles.notificationHeader}>
-            <DefaultText
-              text={userIdFrom}
-              style={isNew ? null : styles.fontColorNotNew}
-            />
-            <DefaultText
-              text={dayjs(createdAt).format(
-                dayjs().isToday(createdAt) ? "HH:MM" : "DD MMM"
-              )}
-              style={styles.notificationDate}
-            />
-          </View>
-          <View style={styles.notificationBody}>
-            <DefaultText
-              text={notification}
-              style={[
-                styles.notificationText,
-                isNew ? null : styles.fontColorNotNew,
-              ]}
-            />
+        <View
+          style={[
+            styles.container,
+            isRead ? styles.containerBackgroundNew : styles.containerBackground,
+          ]}
+        >
+          {isRead ? (
+            <View style={styles.pinkDotBox}>
+              <View style={styles.pinkDot} />
+            </View>
+          ) : null}
+          <View style={styles.notificationBox}>
+            <View style={styles.notificationHeader}>
+              <DefaultText
+                text={userIdFrom}
+                style={isRead ? null : styles.fontColorNotNew}
+              />
+              <DefaultText
+                text={dayjs(createdAt).format(
+                  dayjs().isToday(createdAt) ? "HH:MM" : "DD MMM"
+                )}
+                style={styles.notificationDate}
+              />
+            </View>
+            <View style={styles.notificationBody}>
+              <DefaultText
+                text={notification}
+                style={[
+                  styles.notificationText,
+                  isRead ? null : styles.fontColorNotNew,
+                ]}
+              />
+            </View>
           </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </Swipeable>
   );
 }
 
@@ -107,5 +135,17 @@ const styles = StyleSheet.create({
   },
   fontColorNotNew: {
     color: "#909090",
+  },
+  rightSwipe: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 70,
+    paddingTop: 10,
+    backgroundColor: "#1696E2",
+  },
+  rightSwipeText: {
+    fontSize: 14,
+    color: "#fff",
   },
 });
