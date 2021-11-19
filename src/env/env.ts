a89a12c490826705da18824ce2c5162f3f87ee0a
Polina Karyzhska <polina.k.jsn@gmail.com>
2021-11-19 12:35:28 +0200

    changed logic of occurence of word other

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,4 +1,4 @@
-// export const API_HOST = "https://snaplaw-api.jsninjas.net";
-export const API_HOST = "http://192.168.1.50:4000";
-// export const MEDIA_HOST = "https://snaplaw-api.jsninjas.net/api/media";
-export const MEDIA_HOST = "http://192.168.1.50:4000/api/media";
+export const API_HOST = "https://snaplaw-api.jsninjas.net";
+// export const API_HOST = "http://192.168.1.50:4000";
+export const MEDIA_HOST = "https://snaplaw-api.jsninjas.net/api/media";
+// export const MEDIA_HOST = "http://192.168.1.50:4000/api/media";
